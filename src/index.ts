68b411139568fd44e0fa6b0b5cd7fcd03c73e126
asgervelling <asger.skov.velling@gmail.com>
2024-07-11 13:28:07 +0200

    m

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,21 @@
 import { input } from "@inquirer/prompts";
 
 const answer = await input({ message: "Enter your name" });
+
+// Fun exercise
+type Primitive = string | number | boolean | null;
+type FormatA = { key: string; value: Primitive };
+type FormatB = {
+  [key: string]: Primitive;
+};
+
+function aToB(a: FormatA): FormatB {
+  return {
+    [a.key]: a.value,
+  };
+}
+
+function bToA(b: FormatB): FormatA {
+  const [key, value] = Object.entries(b)[0];
+  return { key, value };
+}
